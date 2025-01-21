f980523d9379c13b04e6d4083924d832a7c4e26a
Tv show <businesskaro0565@gmail.com>
2025-01-21 16:48:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -247,7 +247,7 @@ Nᴀᴍᴇ - {}"""
 
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
-<b>⚙️ sɪᴢᴇ : {file_size} \n\n <blockquote> 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/tvshowbacki">𝗧𝘃 𝘀𝗵𝗼𝘄 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</blockquote> </b>""" 
+<b>⚙️ sɪᴢᴇ : {file_size} \n<blockquote> 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/tvshowbacki">𝗧𝘃 𝘀𝗵𝗼𝘄 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</blockquote> </b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
