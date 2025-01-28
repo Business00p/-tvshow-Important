a172648d63c17e62cff1988f6cf079d5bbdfa800
Tv show <businesskaro0565@gmail.com>
2025-01-28 17:48:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,7 +5,7 @@
 class script(object):
     START_TXT = """<b> ʜᴇʟʟᴏ {}, {} 🌟
     
-I'M YOUR ULTIMATE MOVIE BUDDY. ASK ME FOR ANY MOVIE LET'S FIND YOUR FAVORITE MOVIE TOGETHER! 🕵️  </b>"""
+    I'M YOUR ULTIMATE MOVIE BUDDY.ASK ME FOR ANY MOVIE LET'S FIND YOUR FAVORITE MOVIE TOGETHER!! 🕵️ \n<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/TvShOw076"> ×͜×𝐌𝐫.𝐀𝐦 </a></blockquote> </b>"""
     
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
