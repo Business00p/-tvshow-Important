a2e3152c577a6f553bb2b9429661242f9f6796e5
Tv show <businesskaro0565@gmail.com>
2025-01-28 16:54:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,7 +3,7 @@
 # Ask Doubt on telegram @KingVJ01
 
 class script(object):
-    START_TXT = """<b>Welcome {},\n I am your ultimate movie and series assistant! 🎬 \n\n I'm here to help you find and access your favorite movies or series instantly! 🚀 </b>"""
+    START_TXT = """<b>Welcome {}, {}\n I am your ultimate movie and series assistant! 🎬✨ \n I'm here to help you find and access your favorite movies or series instantly! 🚀 \n <blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/TvShOw076">×͜×𝐌𝐫.𝐀𝐦™ </a></blockquote> </b>"""
     
     
 
